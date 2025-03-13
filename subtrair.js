8659ec41b1fe58a44544684bbb50dbdfe5cb44dc
Enzo <67162943+Enko-Aniki@users.noreply.github.com>
2025-03-13 17:11:06 -0300

    Add files via upload

---
+++ subtrair.js
@@ -0,0 +1,6 @@
+export function subtrair(num1,num2){
+
+    return num1-num2
+
+
+}
---
+++ teste.js
@@ -0,0 +1,8 @@
+import { somar } from "./somar.js";
+
+const num_1 = 5
+const num_2 = 3
+
+let resultado = somar(num_1,num_2);
+
+console.log(resultado)
