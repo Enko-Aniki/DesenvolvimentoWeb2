54770ec674d64d1cacc79d4eb169173fe46d608d
Enzo <67162943+Enko-Aniki@users.noreply.github.com>
2025-03-13 17:13:16 -0300

    Add files via upload

---
+++ atividade2/arrays.js
@@ -0,0 +1,8 @@
+export function Reverse(array){
+    let arrayInvertido = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        arrayInvertido.push(array[i]);
+    }
+    return arrayInvertido;
+    }
+
---
+++ atividade2/idex.js
@@ -0,0 +1,8 @@
+import {Reverse} from "./arrays.js"
+
+
+let arrayO = [1,2,3,4,5];
+let newArray = Reverse(arrayO)
+
+console.log(newArray)
+
